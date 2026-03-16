801d630003cc50e736afaf59770ae7ad0fbbfcd6
Sahuta <ruriboshi6464@yahoo.co.jp>
2026-03-16 08:35:20 +0000

    test

---
+++ app.js
@@ -0,0 +1,59 @@
+let data = []
+let queue = []
+let current = 0
+
+fetch("data.json")
+.then(r=>r.json())
+.then(d=>{
+data = d
+queue = shuffle([...data])
+next()
+})
+
+function shuffle(a){
+for(let i=a.length-1;i>0;i--){
+let j=Math.floor(Math.random()*(i+1))
+[a[i],a[j]]=[a[j],a[i]]
+}
+return a
+}
+
+function next(){
+
+if(queue.length==0){
+queue = shuffle([...data])
+}
+
+let q = queue.shift()
+
+current = q
+
+document.getElementById("japanese").innerText = q.japanese
+document.getElementById("english").innerText = q.english
+document.getElementById("note").innerText = q.note
+
+document.getElementById("english").style.display="none"
+document.getElementById("note").style.display="none"
+
+}
+
+function showAnswer(){
+document.getElementById("english").style.display="block"
+document.getElementById("note").style.display="block"
+}
+
+function markEasy(){
+next()
+}
+
+function markHard(){
+
+queue.splice(
+Math.floor(Math.random()*Math.min(5,queue.length)),
+0,
+current
+)
+
+next()
+
+}
